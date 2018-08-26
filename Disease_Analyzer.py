9a6a7537d8da7ea7808543d808752c72490171f1
Karthikeyan A.S <karthias1997@gmail.com>
2018-08-26 09:49:07 +0530

    updated

--- Disease_Analyzer.py
+++ Disease_Analyzer.py
@@ -67,36 +67,9 @@ if __name__ == "__main__":
   input_layer = "Mul"
   output_layer = "final_result"
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--image", help="image to be processed")
-  parser.add_argument("--graph", help="graph/model to be executed")
-  parser.add_argument("--labels", help="name of file containing labels")
-  parser.add_argument("--input_height", type=int, help="input height")
-  parser.add_argument("--input_width", type=int, help="input width")
-  parser.add_argument("--input_mean", type=int, help="input mean")
-  parser.add_argument("--input_std", type=int, help="input std")
-  parser.add_argument("--input_layer", help="name of input layer")
-  parser.add_argument("--output_layer", help="name of output layer")
-  args = parser.parse_args()
+ 
 
-  if args.graph:
-    model_file = args.graph
-  if args.image:
-    file_name = args.image
-  if args.labels:
-    label_file = args.labels
-  if args.input_height:
-    input_height = args.input_height
-  if args.input_width:
-    input_width = args.input_width
-  if args.input_mean:
-    input_mean = args.input_mean
-  if args.input_std:
-    input_std = args.input_std
-  if args.input_layer:
-    input_layer = args.input_layer
-  if args.output_layer:
-    output_layer = args.output_layer
+  file_name="tom_mos_vir.jpg"
 
   graph = load_graph(model_file)
   t = read_tensor_from_image_file(file_name,
